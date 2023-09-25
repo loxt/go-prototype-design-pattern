9ebc050206da22732f2d571c2503887c48f6cf4d
emannuel <emannuelloxt@hotmail.com>
2023-09-24 20:25:40 -0400

    feat: copy through serialization

---
+++ copy_through_serialization/copy_through_serialization.go
@@ -0,0 +1,27 @@
+package copy_through_serialization
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+type Address struct {
+	StreetAddress, City, Country string
+}
+
+type Person struct {
+	Name    string
+	Address *Address
+	Friends []string
+}
+
+func (p *Person) DeepCopy() *Person {
+	b := bytes.Buffer{}
+	e := gob.NewEncoder(&b)
+	_ = e.Encode(p)
+
+	d := gob.NewDecoder(&b)
+	result := Person{}
+	_ = d.Decode(&result)
+	return &result
+}
